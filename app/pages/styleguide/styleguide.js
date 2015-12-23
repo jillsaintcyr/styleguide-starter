57d7c1d162f80f63067583b7300a04e22ed53bff
Jill Baker <jill.baker@digitas.com>
2015-12-23 16:47:48 -0500

    adding prism, continued work on ref pages

--- app/pages/styleguide/styleguide.js
+++ app/pages/styleguide/styleguide.js
@@ -5,7 +5,11 @@ var APP = window.APP = window.APP || {};
 APP.styleguide = (function () {
 
     var bindEventsToUI = function () {
-
+        $('body').on('click', '.reference-nav__item a', function() {
+            var destination = $(this).attr('href');
+            $('.reference-section').not( $(destination) ).slideUp();
+            $(destination).delay(300).fadeIn();
+        });
     };
 
     var init = function (element) {
